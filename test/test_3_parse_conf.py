5b76776c433501938bbab938a71dc8b37876a004
Roland Hedberg <roland.hedberg@adm.umu.se>
2016-02-16 10:22:51 +0100

    Allow configuration in JSON.

--- test/test_3_parse_conf.py
+++ test/test_3_parse_conf.py
@@ -1,5 +1,6 @@
 from aatest import func
 from aatest import operation
+from aatest.parse_cnf import parse_json_conf
 from aatest.parse_cnf import parse_yaml_conf
 
 __author__ = 'roland'
@@ -8,6 +9,17 @@ __author__ = 'roland'
 def test_parse_yaml_conf():
     factories = {'': operation.factory}
     cnf = parse_yaml_conf('flows.yaml', factories, func.factory)
+
+    assert cnf
+
+    assert cnf['Desc'] == {'Default': 'Default settings'}
+    assert list(cnf['Flows'].keys()) == ['AA-Dummy-Default-1']
+    assert cnf['Order'] == ['AA-Dummy-Default']
+
+
+def test_parse_json_conf():
+    factories = {'': operation.factory}
+    cnf = parse_json_conf('flows.json', factories, func.factory)
 
     assert cnf
 
