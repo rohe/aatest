1792dca07f339a73452fd798f7a9be2d1427d9bf
Roland Hedberg <roland.hedberg@adm.umu.se>
2016-05-08 20:44:27 +0200

    New ProtocolMessage class

--- src/aatest/operation.py
+++ src/aatest/operation.py
@@ -187,6 +187,23 @@ class TimeDelay(Operation):
         return None
 
 
+class ProtocolMessage(object):
+    def __init__(self, conv, req_args, binding, msg_param=None):
+        self.conv = conv
+        self.entity = conv.entity
+        self.req_args = req_args
+        self.binding = binding
+        self.msg_param = msg_param or {}
+        self.response_args = {}
+        self.op_args = {}
+
+    def construct_message(self, *args):
+        raise NotImplementedError()
+
+    def handle_response(self, result, *args):
+        raise NotImplementedError()
+
+
 def factory(name):
     for fname, obj in inspect.getmembers(sys.modules[__name__]):
         if inspect.isclass(obj):
